dc36221d73d077857b4c099356a8ce45e9aedbb9
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-04-30 13:55:57 +0330

    new update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+
+function bayern(apple,orange) {
+    console.log(apple,orange);
+    const juice = `${apple} apple and ${orange} orange`
+    return juice
+}
+
+console.log(bayern(2,4));
