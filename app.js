6ec8a4ce0faf5ec39231bb119dda045ef847f93d
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-04-30 14:12:52 +0330

    new update

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 'use strict'
 
 
-function bayern(apple,orange) {
-    console.log(apple,orange);
-    const juice = `${apple} apple and ${orange} orange`
-    return juice
+// function bayern(apple,orange) {
+//     console.log(apple,orange);
+//     const juice = `${apple} apple and ${orange} orange`
+//     return juice
+// }
+
+// console.log(bayern(2,4));
+
+
+function culAge1(age){
+    return 2037 - age
+} 
+
+
+const culAge2 = function (age){
+    return 2037 - age
 }
 
-console.log(bayern(2,4));
+
+
+console.log(culAge1(2000));
+console.log(culAge2(1999));
