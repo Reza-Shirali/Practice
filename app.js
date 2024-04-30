e07564208ccb10e1068dcefae28775c2b1938634
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-04-30 15:56:27 +0330

    new update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-'use strict'
-
+"use strict";
 
 // function bayern(apple,orange) {
 //     console.log(apple,orange);
@@ -9,21 +8,16 @@
 
 // console.log(bayern(2,4));
 
-
 // function culAge1(age){
 //     return 2037 - age
-// } 
-
+// }
 
 // const culAge2 = function (age){
 //     return 2037 - age
 // }
 
-
-
 // console.log(culAge1(2000));
 // console.log(culAge2(1999));
-
 
 // const printName = (firstName) =>{
 //    return `My name is ${firstName}`
@@ -31,16 +25,41 @@
 
 // console.log(printName("Reza"));
 
-const cutFruitPieces = (fruit) =>{
-    return fruit * 4
+// const cutFruitPieces = (fruit) =>{
+//     return fruit * 4
+// }
+
+// const fruitProcessor = (apples,oranges) =>{
+//     const applePieces = cutFruitPieces(apples)
+//     const orangePieces = cutFruitPieces(oranges)
+//     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`
+//     return juice
+// }
+
+// console.log(fruitProcessor(2,3));
+
+
+const scoreDolphins = [44,23,71]
+const scoreKoalas = [65,54,49]
+
+// const scoreDolphins = [85,54,41]
+// const scoreKoalas = [23,34,27]
+const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
+  const averageScore = (scoreOne + scoreTwo + scoreThree) / 3;
+  return averageScore;
+};
+
+const aveDolphins = calcAverage(scoreDolphins[0],scoreDolphins[1],scoreDolphins[2])
+const aveKoalas = calcAverage(scoreKoalas[0],scoreKoalas[1],scoreKoalas[2])
+
+const checkWinner = (aveDolphins , aveKoalas) =>{
+    if(aveDolphins >= 2*aveKoalas){
+        console.log(`Dolphins win (${aveDolphins} Vs ${aveKoalas})`);
+    }else if(aveKoalas >= 2*aveDolphins ){
+        console.log(`Koalas win (${aveKoalas} Vs ${aveDolphins})`);
+    }else{
+        console.log(`No Team Wins...`);
+    }
 }
 
-
-const fruitProcessor = (apples,oranges) =>{
-    const applePieces = cutFruitPieces(apples)
-    const orangePieces = cutFruitPieces(oranges)
-    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange` 
-    return juice
-}
-
-console.log(fruitProcessor(2,3));
+checkWinner(aveDolphins,aveKoalas)
