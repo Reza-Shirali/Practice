a7e573fc6d88240b0043f02df7785e321e9c2f87
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-05-01 16:53:01 +0330

    new update

--- app.js
+++ app.js
@@ -39,27 +39,37 @@
 // console.log(fruitProcessor(2,3));
 
 
-const scoreDolphins = [44,23,71]
-const scoreKoalas = [65,54,49]
+// const scoreDolphins = [44,23,71]
+// const scoreKoalas = [65,54,49]
 
-// const scoreDolphins = [85,54,41]
-// const scoreKoalas = [23,34,27]
-const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
-  const averageScore = (scoreOne + scoreTwo + scoreThree) / 3;
-  return averageScore;
-};
+// // const scoreDolphins = [85,54,41]
+// // const scoreKoalas = [23,34,27]
+// const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
+//   const averageScore = (scoreOne + scoreTwo + scoreThree) / 3;
+//   return averageScore;
+// };
 
-const aveDolphins = calcAverage(scoreDolphins[0],scoreDolphins[1],scoreDolphins[2])
-const aveKoalas = calcAverage(scoreKoalas[0],scoreKoalas[1],scoreKoalas[2])
+// const aveDolphins = calcAverage(scoreDolphins[0],scoreDolphins[1],scoreDolphins[2])
+// const aveKoalas = calcAverage(scoreKoalas[0],scoreKoalas[1],scoreKoalas[2])
 
-const checkWinner = (aveDolphins , aveKoalas) =>{
-    if(aveDolphins >= 2*aveKoalas){
-        console.log(`Dolphins win (${aveDolphins} Vs ${aveKoalas})`);
-    }else if(aveKoalas >= 2*aveDolphins ){
-        console.log(`Koalas win (${aveKoalas} Vs ${aveDolphins})`);
-    }else{
-        console.log(`No Team Wins...`);
-    }
-}
+// const checkWinner = (aveDolphins , aveKoalas) =>{
+//     if(aveDolphins >= 2*aveKoalas){
+//         console.log(`Dolphins win (${aveDolphins} Vs ${aveKoalas})`);
+//     }else if(aveKoalas >= 2*aveDolphins ){
+//         console.log(`Koalas win (${aveKoalas} Vs ${aveDolphins})`);
+//     }else{
+//         console.log(`No Team Wins...`);
+//     }
+// }
 
-checkWinner(aveDolphins,aveKoalas)
+// checkWinner(aveDolphins,aveKoalas)
+
+const arrayOne  = ["reza",24,"FrontEnd Developer"]
+console.log(arrayOne);
+
+arrayOne.push("bayern")
+console.log(arrayOne);
+
+arrayOne.unshift("Esteghlal")
+console.log(arrayOne);
+
