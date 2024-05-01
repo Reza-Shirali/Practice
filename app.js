6269c84e4961dc03e91d0f003a9592b15d18b47e
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-05-01 19:11:22 +0330

    new update

--- app.js
+++ app.js
@@ -78,17 +78,36 @@
 // arrayOne.shift()
 // console.log(arrayOne);
 
-const calcTip = (bill) => {
-    if(bill > 50 && bill <300){
-        return bill * 0.15
-    }else{
-        return bill * 0.2
-    }
+// const calcTip = (bill) => {
+//   if (bill > 50 && bill < 300) {
+//     return bill * 0.15;
+//   } else {
+//     return bill * 0.2;
+//   }
+// };
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(tips);
+// console.log(total);
+
+const myObj = {
+  firstName: "reza",
+  lastName:"Shirali",
+  age: 2024-2000,
+  job: "Front End Developer"
 }
 
-const bills = [125,555,44]
-const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
-const total = [bills[0] + tips[0] , bills[1] + tips[1] , bills[2] + tips[2]]
+const interestedIn = prompt(`what do you want to know about reza? Choose between firstName , lastName , age , job`)
+console.log(interestedIn);
 
-console.log(tips);
-console.log(total);
+alert(myObj[interestedIn])
+
+// console.log(myObj.age);
+// console.log(myObj['lastName']);
+
+// const name = "Name"
+
+// console.log(myObj['first' + name]);
