9d04838ea64394018c4f8d99be2acb50ef834fcc
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-05-02 16:58:25 +0330

    new update

--- app.js
+++ app.js
@@ -272,14 +272,29 @@
 
 // console.log(amplitude);
 
-const measureKelvin = () =>{
-  const measurement = {
-    type : 'temp',
-    unit : 'celcius',
-    value: +prompt(`Degrees Celcius:`)
-  }
-  const kelvin = measurement.value + 273
-  return kelvin 
-} 
+// const measureKelvin = () =>{
+//   const measurement = {
+//     type : 'temp',
+//     unit : 'celcius',
+//     value: +prompt(`Degrees Celcius:`)
+//   }
+//   const kelvin = measurement.value + 273
+//   return kelvin
+// }
 
-console.log(measureKelvin());
+// console.log(measureKelvin());
+
+const testOne = [17, 21, 23];
+const testTwo = [12, 5, -5, 0, 4];
+
+const printForecast = (arr) => {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days... `;
+  }
+  console.log('... '+str);
+};
+console.log(`--- Test One ---`)
+printForecast(testOne)
+console.log(`--- Test Two ---`);
+printForecast(testTwo)
