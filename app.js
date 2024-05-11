2984581d14b4fc29193c8e792610220207bc7a6d
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-05-11 10:56:03 +0330

    New commit

--- app.js
+++ app.js
@@ -492,24 +492,99 @@ const restaurant = {
 // const guestCorrect = restaurant.numGuests ?? 10;
 // console.log(guestCorrect);
 
-const restOne = {
-  name: "Capri",
-  // numGuests: 20,
-  numGuests : 0
+// const restOne = {
+//   name: "Capri",
+//   // numGuests: 20,
+//   numGuests : 0
+// };
+
+// const restTwo = {
+//   name: "La Piazza",
+//   owner: "Giovanni Rossi",
+// };
+
+// restOne.numGuests ??= 10;
+// restTwo.numGuests ??= 10;
+// console.log(restOne.numGuests);
+// console.log(restTwo.numGuests);
+
+// restOne.owner &&= "ANONYMOUS"
+// restTwo.owner &&= "ANONYMOUS"
+// console.log(restOne);
+// console.log(restTwo);
+
+// CHALLENGE
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
-const restTwo = {
-  name: "La Piazza",
-  owner: "Giovanni Rossi",
+// .1
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// .2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// .3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// .4
+const playersFinal1 = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(playersFinal1);
+
+// .5
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+// .6
+
+const printGoals = (...players) => {
+  console.log(players);
+  console.log(`${players.length} goals were scored.`);
 };
 
-restOne.numGuests ??= 10;
-restTwo.numGuests ??= 10;
-console.log(restOne.numGuests);
-console.log(restTwo.numGuests);
+printGoals(...game.scored);
 
-
-restOne.owner &&= "ANONYMOUS"
-restTwo.owner &&= "ANONYMOUS"
-console.log(restOne);
-console.log(restTwo);
+// .7
+ team1 > team2 && console.log(`Team 2 is more likely to win`);
+ team1 < team2 && console.log(`Team 1 is more likely to win`);
